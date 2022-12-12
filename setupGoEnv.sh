7723c6691c58e16d7a382bcfa43bd2767767b175
OC <hi@xiaoli.dev>
2022-12-12 10:49:53 +0800

    added the rust-lanage packages

--- setupGoEnv.sh
+++ setupGoEnv.sh
@@ -8,5 +8,16 @@ wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-echo " $leftChar Checking whether it's installed? $rightChar "
+source "$HOME"/.bashrc
+
+sleep 10
+wait $!
+
+echo "$leftChar Starting to download Rust packages from official website at $(date) $rightChar "
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME"/.bashrc
+
+echo " $leftChar Checking whether all the packages installed? $rightChar "
 go version
+cargo version
+rustc version
