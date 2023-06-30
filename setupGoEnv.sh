0fe0397134cb88e7565716fd642a68aab83e3a5d
OC-XIAOLI <hi@xiaoli.dev>
2023-06-30 16:07:48 +0800

    changed go version to 1.20.5

--- setupGoEnv.sh
+++ setupGoEnv.sh
@@ -4,8 +4,8 @@ leftChar="⇢ |・・・・・・ "
 rightChar=" ・・・・・・ | ⇠"
 
 echo " $leftChar Starting to download Golang packages from official website at $(date) $rightChar "
-wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 source "$HOME"/.bashrc
