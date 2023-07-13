fa04ff84ce4a99583410099b593891db3b46996d
OC-XIAOLI <hi@xiaoli.dev>
2023-07-13 15:36:05 +0800

    fixed source command

--- setupGoEnv.sh
+++ setupGoEnv.sh
@@ -8,7 +8,8 @@ wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-source "$HOME"/.bashrc
+source $HOME/.bashrc
+source $HOME/.profile
 
 sleep 10
 wait $!
